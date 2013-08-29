289bad48da43fff24e74422498e3ec4a40533ae8
Kerry Street <kerrycstreet@gmail.com>
2013-08-28 19:56:00 -0700

    add WP PCL core profile 78+WP8 with Mvx

---
+++ Facebook.SampleApp.WindowsPhone/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace Facebook.SampleApp.WindowsPhone.Views
+{
+    public partial class FirstView : MvxPhonePage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}

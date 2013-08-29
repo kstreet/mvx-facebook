051da87995c8ac03debda0e533e119b4ea6825df
Kerry Street <kerrycstreet@gmail.com>
2013-08-28 22:22:25 -0700

    LoginView Mods for testing

---
+++ Facebook.SampleApp.WindowsPhone/GlobalVariablesForTesting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook.SampleApp.WindowsPhone
+{
+    public static class GlobalVariablesForTesting
+    {
+        public static string CurrentUserId { get; set; }
+        public static string CurrentAzureToken { get; set; }
+    }
+}
--- Facebook.SampleApp.WindowsPhone/Views/LoginView.xaml.cs
+++ Facebook.SampleApp.WindowsPhone/Views/LoginView.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using BeingTheWorst.MvxPlugins.AzureMobileAuthN.ViewModels;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
 using Cirrious.MvvmCross.WindowsPhone.Views;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
@@ -16,6 +21,36 @@ namespace Facebook.SampleApp.WindowsPhone.Views
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Use this code behind hack to take the AuthN data and put it somwhere
+            // this running App can get access to for testing.
+            Debug.WriteLine("In the Facebook LoginView: {0}, {1}", this.UserId.Text, this.AzureToken.Text);
+
+            GlobalVariablesForTesting.CurrentUserId = this.UserId.Text;
+            GlobalVariablesForTesting.CurrentUserId = this.AzureToken.Text;
+
+            // Navigate to a new Facebook Test Views from here.  See Nav hack below
+            var nav = new NavigateMyselfService();
+            nav.GoWild();
+
+        }
+    }
+
+    public interface INavigateMyselfService
+    {
+        void GoWild();
+    }
+
+    public class NavigateMyselfService
+       : MvxNavigatingObject
+       , INavigateMyselfService
+    {
+        public void GoWild()
+        {
+            ShowViewModel<HomeViewModel>();
         }
     }
 }
